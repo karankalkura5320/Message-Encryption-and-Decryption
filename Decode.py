34c722f70958b0761324aeae78a7bd08c4e526c0
karankalkura5320 <84461122+karankalkura5320@users.noreply.github.com>
2021-05-19 21:41:40 +0530

    Add files via upload

---
+++ Decode.py
@@ -0,0 +1,20 @@
+#Decode
+
+shift = int(input('enter the shift:'))
+
+data= input('enter the data:')
+
+for i in data:
+    if i==' ':
+        print('', end='')
+    else:
+        if i.isupper():
+            if (ord(i)-shift) > 64:
+                print(chr(ord(i)-shift), end='')
+            elif (ord(i)-shift) <= 64:
+                print(chr(ord(i)-shift+26), end='')
+        elif i.islower():
+            if (ord(i)-shift) > 96:
+                print(chr(ord(i)-shift), end='')
+            elif (ord(i)-shift) <= 96:
+                print(chr(ord(i)-shift+26), end='')
